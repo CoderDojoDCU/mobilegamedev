1551d62ff28e2912dece710c9b90bb5f97b2dd58
Noel King <noelking@gmail.com>
2015-11-27 20:46:42 +0000

    Week 7 added the running ninja

--- www/js/jumpy.js
+++ www/js/jumpy.js
@@ -1,9 +1,16 @@
 var cylinder;
 var screenHeight = screen.height;
 var jumpBy = 10;
+var ninja;
+var screenWidth = screen.width;
+var runBy = 10;
 
 function startGame() {
 	cylinder = 	$("#cylinder");
+    ninja = $("#ninja");
+    ninja.click(function() {
+        run();
+    });
 	jump(10);
 }
 
@@ -15,10 +22,27 @@ function jump() {
     
     if(bottomPosition >= screenHeight) {
         cylinder.css('top','0px');
+        setRandomLeft();
     } else {
         cylinder.css('top', newPosition+"px");
     }
     setTimeout(jump, 30);
+}
+
+function setRandomLeft() {
+    var leftPosition = Math.floor(Math.random() * 50) + 20;
+    cylinder.css('margin-left', leftPosition + "%");
+}
+
+function run() {
+    var ninjaDimension = ninja.offset();
+    var ninjaLeft = ninjaDimension.left;
+    var newLeftPosition = ninjaLeft+runBy;
+    console.log(newLeftPosition);
+    ninja.css('left', newLeftPosition+"px");
+    
+    run();
+
 }
 
 startGame();
